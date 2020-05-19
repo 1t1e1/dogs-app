f1664b0343d458734fdadc8bd0d0b4ae3d413f75
1t1e1 <1t1ebir@gmail.com>
2020-05-19 03:54:48 +0300

    dogsData eklendi. isimleri liste olarak gosterildi.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import dogsData from "./dogsdata";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ul>
+                {dogsData.map((dog) => (
+                    <li>{dog.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
