e4a0c5ea6cc66d61a84477f0a4adf65f1437c44c
1t1e1 <1t1ebir@gmail.com>
2020-05-19 05:22:58 +0300

    bootstrap eklendi. Duzeltme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+import Header from "./Header";
 import dogsData from "./dogsdata";
 
 function App() {
     return (
         <div className="App">
+            <Header></Header>
             <ul>
                 {dogsData.map((dog) => (
-                    <li>{dog.name}</li>
+                    <li>
+                        <Button>{dog.name}</Button>
+                    </li>
                 ))}
             </ul>
         </div>
