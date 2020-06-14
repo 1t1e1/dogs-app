f05eb8483825767e851c037e88b24958be6873a2
1t1e1 <1t1ebir@gmail.com>
2020-06-14 21:26:41 +0300

    404 sayfasi eklendi

--- src/routes.js
+++ src/routes.js
@@ -3,23 +3,32 @@ import Homepage from "./containers/Homepage";
 import FilteredDogList from "./containers/FilteredDogList";
 
 const routes = [
-    {
-        path: "/",
-        isExact: true,
-        component: Homepage,
-        title: "Dogs App",
-    },
-    {
-        path: "/tur/:yazilanTur",
-        isExact: true,
-        component: FilteredDogList,
-    },
-    {
-        path: "/about",
-        isExact: false,
-        component: () => <div> hakimizda </div>,
-        title: "About",
-    },
+	{
+		path: "/",
+		isExact: true,
+		component: Homepage,
+		title: "Dogs App",
+	},
+	{
+		path: "/tur/:yazilanTur",
+		isExact: true,
+		component: FilteredDogList,
+	},
+	{
+		path: "/about",
+		isExact: false,
+		component: () => <div> hakimizda </div>,
+	},
+	{
+		path: "/:notFound",
+		isExact: false,
+		component: () => (
+			<div>
+				{" "}
+				<h1> Error 404 not found</h1>
+			</div>
+		),
+	},
 ];
 
 export default routes;
